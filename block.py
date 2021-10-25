165ccdf27089c94b49cb4f8d29df1b56fa963a20
d0ct0r3v1lk5h1t1j <56975193+d0ct0r3v1lk5h1t1j@users.noreply.github.com>
2021-10-25 12:43:08 +0530

    Add files via upload

--- block.py
+++ block.py
@@ -3,62 +3,76 @@ import os
 import hashlib
 import time
 
-block_direc = 'blockchain/'
+block_direc = 'blockchain/'  # constant path for file directory of json file of blocks
 
-def get_hash(prev_block) :
+
+def get_hash(prev_block):  # function to get hash for previous block
+    # reading the previous block in binary format
     with open(block_direc + prev_block, 'rb') as f:
-        content  = f.read()
+        content = f.read()
+    # hashlib library to return hash of binary content of previous block
     return hashlib.md5(content).hexdigest()
 
 
-def check_integrity() : 
-    files = sorted(os.listdir(block_direc), key = lambda x : int(x))
-    
+def check_integrity():  # function to check if any previous verified data was overwritten
+    files = sorted(os.listdir(block_direc), key=lambda x: int(x))
+
     results = []
 
-    for file in files[1:] :
-        with open(block_direc + file ) as f:
+    for file in files[1:]:
+        with open(block_direc + file) as f:
             block = json.load(f)
 
             prev_hash = block.get('prev_block').get('hash')
             prev_filename = block.get('prev_block').get('filename')
             timest = block.get('timestamp')
             actual_hash = get_hash(prev_filename)
-            if prev_hash == actual_hash :
-               res = 'OK'
-            else :
+            if prev_hash == actual_hash:  # comparing hash value of current block with the hash value stored in next block
+                res = 'OK'
+            else:
                 res = 'Was Changed'
-              
-            print(f'block {prev_filename} : {timest}')
-            results.append({'block' : prev_filename ,  'timestamp' : timest})
+
+            print(f'block {prev_filename} : {res} : {timest}')
+            results.append(
+                {'block': prev_filename, 'result': res, 'timestamp': timest})
 
     return results
 
 
-def write_block(borrower, lender, amount, time=time.ctime()):
-   block_count =  len(os.listdir(block_direc))
+# function to verify uncomfirmed transaction and store them in our blockchain
+def write_block(transactions, time=time.ctime()):
+    block_count = len(os.listdir(block_direc))
 
-   prev_block = str(block_count)
-   data =  {  
-    "borrower" : borrower ,
-    "lender" : lender ,
-    "amount" : amount ,
-    "prev_block" : {
-        "hash" : get_hash(prev_block) ,
-        "filename" : prev_block
-    },
-    "timestamp": str(time)
+    prev_block = str(block_count)
+    transactions['prev_block'] = {
+        "hash": get_hash(prev_block),
+        "filename": prev_block
     }
+    transactions['timestamp'] = str(time)
     
-   current_block = block_direc +  str(block_count + 1)
+#    data =  {                                                                          #storing data in json format
+#     "borrower" : borrower ,
+#     "lender" : lender ,
+#     "amount" : amount ,
+#     "prev_block" : {
+#         "hash" : get_hash(prev_block) ,
+#         "filename" : prev_block
+#     },
+#     "timestamp": str(time)
+#     }
 
-   with open(current_block, 'w') as f :
-     json.dump(data, f, indent = 4, ensure_ascii = False)
-     f.write('\n')
+    current_block = block_direc + str(block_count + 1)
+    data = json.dumps(transactions, indent=4)
+
+    with open(current_block, 'w') as f:
+        f.write(data)
+    #  json.dump(data, f, indent = 4, ensure_ascii = False)                          #ensuring encyption of the json file with 4 bytes of indent
+        # newline for stroing next data
+
 
 def main():
-    write_block(borrower= "Kshitij", lender= "Gaurvit", amount= 100)
     check_integrity()
 
-if __name__ == '__main__' :
+
+if __name__ == '__main__':
     main()
--- main.py
+++ main.py
@@ -6,9 +6,19 @@ import time
 
 app = Flask(__name__)
 
-transac = [] # string string int
+"""
+transac = {
+    'data': [],
+    'hash_details': {}
+}
+"""
+transac = {
+    'data': [],
+}                                                                   # a list to store unverified transactions
 
-@app.route('/', methods=['POST','GET'])
+
+# Standard method of flask to get input from the page
+@app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
         borrower = request.form.get('borrower')
@@ -16,33 +26,30 @@ def index():
         amount = request.form.get('amount')
 
         transtime = time.ctime()
-        transac.append({'borrower' : borrower, 'lender' : lender, 'amount' : amount, 'transtime' : transtime})
+        # storing transaction details with transaction time into the list
+        transac['data'].append(
+            {'borrower': borrower, 'lender': lender, 'amount': amount, 'transtime': transtime})
 
-#        write_block(borrower = borrower,lender=lender, amount=amount)
+    return render_template('index.html', transax=transac)
 
-    return render_template('index.html', transax = transac)
 
+# function for mine button to call mine function
 @app.route('/mine')
 def mine():
     if request.method == 'GET':
-        for x in transac:
-            write_block(borrower = x.get('borrower'), lender = x.get('lender'), amount = x.get('amount'))
-    transac.clear()
+        # adding new blocks to the blockchain , verifying unconfirmed transactions
+        write_block(transac)
+    transac['data'].clear()
     return render_template('index.html')
-    
-    
-    
 
 
+# integrity check for already verified blocks
 @app.route('/checking')
 def check():
-    results= check_integrity()
+    results = check_integrity()
     return render_template('index.html', checking_results=results)
 
 
+# web page works on localhost port 5000
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
-
-
-
-
